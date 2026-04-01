419d8b670b8b114e3d50b3e65dc6262a81ec1bfb
gvisiedo <gregoriovisiedoreche@gmail.com>
2026-04-01 15:53:33 +0200

    working on css and starting with js

--- Calculadora/js/scripts.js
+++ Calculadora/js/scripts.js
@@ -1,3 +1,9 @@
+let pantalla = '0';
+let numAnterior = null;
+let operacion = null;
+let esperandoSegundoNum = false;
+
+const display = document.querySelector('.pantalla');
 
 document.querySelectorAll('.btn-numero').forEach(btn =>{
     btn.addEventListener('click', ()=> pulsarNumero(btn.textContent));
@@ -6,9 +12,26 @@ document.querySelectorAll('.btn-operador').forEach(btn =>{
     btn.addEventListener('click', ()=> pulsarOperador(btn.textContent));
 });
 
-document.querySelectorAll('.btn-igual').addEventListener('click', calcularResultado);
-document.querySelectorAll('.btn-borrar').addEventListener('click', limpiar);
+document.querySelector('.btn-igual').addEventListener('click', calcularResultado);
+document.querySelector('.btn-borrar').addEventListener('click', limpiar);
 
 
+function pulsarNumero(num){
+    display.textContent= num;
+};
+
+function pulsarOperador(op){
+    document.getElementsByClassName(".btn-numero") + document.getElementsByClassName(".btn-operador")
+
+}
+function calcularResultado(){}
+
+function limpiar(){
+    pantalla = '0';
+    numAnterior = null;
+    operacion = null;
+    esperandoSegundoNum = false;
+    display.textContent = '0';
+}
 
 
